11e128748ab9bb3df4382c65e22fda15ee888ce7
WagnerMauro96 <wagnermauro96@gmail.com>
2018-10-24 17:38:31 +0100

    Configuração do Firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseconfig: {
+    apiKey: 'AIzaSyC2kg5bEAuDmlvep1C_qp2xp3j2Zi-XYzE',
+    authDomain: 'auth-ap.firebaseapp.com',
+    databaseURL: 'https://auth-ap.firebaseio.com',
+    projectId: 'auth-ap',
+    storageBucket: 'auth-ap.appspot.com',
+    messagingSenderId: '349967012268'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseconfig: {
+    apiKey: 'AIzaSyC2kg5bEAuDmlvep1C_qp2xp3j2Zi-XYzE',
+    authDomain: 'auth-ap.firebaseapp.com',
+    databaseURL: 'https://auth-ap.firebaseio.com',
+    projectId: 'auth-ap',
+    storageBucket: 'auth-ap.appspot.com',
+    messagingSenderId: '349967012268'
+  }
 };
 
 /*
